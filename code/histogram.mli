8d0ad6a449b23f21ede57c761ee3a13473e899d3
Alex Clemmer <clemmer.alexander@gmail.com>
2014-06-28 21:14:18 -0700

    Add comments to the histogram interface

--- code/histogram.mli
+++ code/histogram.mli
@@ -5,10 +5,24 @@ open Core.Std
 
 type 'a t
 
+(* An empty Histogram.t *)
 val empty     : 'a t
+
+(* Generate a Histogram.t from count data that already exists *)
 val create    : ('a, int) Map.Poly.t -> 'a t
+
+(* Returns the count for some key *)
 val find      : 'a t -> 'a -> int option
+
+(* Increment the count for some key, or initialize it to 1 if it hasn't occurred
+ * yet *)
 val touch     : 'a t -> 'a -> 'a t
+
+(* Touch all keys in a list *)
 val touch_all : 'a t -> 'a list -> 'a t
+
+(* Iterate over, and perform some action on, each item in the Histogram.t *)
 val iter      : 'a t -> f:(key:'a -> data:int -> unit) -> unit
+
+(* Convert the Histogram.t to a list *)
 val to_alist  : 'a t -> ('a * int) list
